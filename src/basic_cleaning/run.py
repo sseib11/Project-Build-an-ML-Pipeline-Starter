bdda7bae58930a536e14317cecb48ecd9c1f0530
sseib11 <sseib11@wgu.edu>
2026-02-07 15:54:32 -0500

    Fix: drop listings outside NYC boundaries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,8 +35,11 @@ def go(args):
     # Only implement this step when reaching Step 6: Pipeline Release and Updates
     # in the project.
     # Add longitude and latitude filter to allow test_proper_boundaries to pass
-    # idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
-    # df = df[idx].copy()
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     # Save the cleaned data
     output_filename = "clean_sample.csv"
